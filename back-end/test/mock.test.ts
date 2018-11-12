c5cdc36ec2395106cadb5a9a2183c5c62e59c578
Uladzislau Kavalski <uladzislau.kavalski@instinctools.ru>
2018-11-12 15:21:07 +0300

    Testing graphql

--- back-end/test/mock.test.ts
+++ back-end/test/mock.test.ts
@@ -1,29 +1,30 @@
 import {} from 'ts-jest';
-import * as supertest from 'supertest';
+// import * as supertest from 'supertest';
+import * as chai from 'chai';
+import 'chai-http';
 import server from '../src/index';
 
-/*
-  let app: AppServer;
-
-  beforeEach(() => {
-    app = server;
-  });
-
-  afterEach(() => {
-    app.close();
-  });
- */
+chai.use(require('chai-http'));
 
 describe('User tests', () => { 
-  test('Get all users from DB', () => {
-    const request = supertest(server);
-    const query = { query: 'query findAll { findAll { email firstName lastName roleId } }' };
+  it('Get all users from DB', (done) => {
+    // const request = supertest(server);
 
-    request
+    // ?query={findAll{email%20firstName%20lastName%20roleId}}
+    /*
+          // .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json; utf-8')
+     */
+
+    chai.request(server)
       .post('/graphql')
-      .send(query)
-      .end(res => {
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json; utf-8')
+      .set('Connection', 'keep-alive')
+      .send('{ "query": "findAll{email firstName lastName roleId}" }')
+      .end((err, res) => {
         console.log(res);
+        done();
       });
   });
 });
