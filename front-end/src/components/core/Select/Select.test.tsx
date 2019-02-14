452f15880c326936e375e4677a631de4a0bf92e1
Roman Gordienko <r.u.gordienko@gmail.com>
2019-02-14 15:42:48 +0300

    tests for Select and Spinner components

---
+++ front-end/src/components/core/Select/Select.test.tsx
@@ -0,0 +1,72 @@
+import { shallowWithTheme, mountWithTheme } from '../../../utils/enzymeHelpersWithPassedTheme';
+import * as React from 'react';
+import { Select, SelectVariants } from './Select';
+
+const state = {
+  value: null,
+};
+
+const requiredProps = {
+  variant: SelectVariants.Default,
+  options: [
+    { value: '1', label: '1' },
+    { value: '2', label: '2' },
+    { value: '3', label: '3' },
+  ],
+  onChange: value => {
+    state.value = value;
+  },
+};
+
+const optionalProps = {
+  placeholder: 'test-placeholder',
+  disabled: true,
+  value: 'stub value',
+  label: 'test label',
+};
+
+afterEach(() => state.value = null);
+
+describe('Select core component:', () => {
+  it('shallow renders without crashing', () => {
+    shallowWithTheme(<Select {...requiredProps} />);
+  });
+
+  it(`passes event.target.value to onChange prop as a parameter, when select changed`, () => {
+    const mockValue = 'mockValue';
+    const shallowWrapper = mountWithTheme(<Select {...requiredProps} />);
+    shallowWrapper.find('select').first().simulate('change', { target: { value: mockValue} });
+
+    expect(state.value).toEqual(mockValue);
+  });
+
+  it('sets select\'s placeholder based on "placeholder" prop value', () => {
+    const mountWrapper = mountWithTheme(<Select {...requiredProps} placeholder={optionalProps.placeholder} />);
+
+    expect(mountWrapper.find('select').first().props().placeholder).toEqual(optionalProps.placeholder);
+  });
+
+  it('disable select when "disabled" prop set to true', () => {
+    const mountWrapper = mountWithTheme(<Select {...requiredProps} disabled={optionalProps.disabled} />);
+
+    expect(mountWrapper.find('select').first().props().disabled).toEqual(optionalProps.disabled);
+  });
+
+  it('sets default select value based on "value" prop', () => {
+    const mountWrapper = mountWithTheme(<Select {...requiredProps} value={optionalProps.value} />);
+
+    expect(mountWrapper.find('select').first().props().value).toEqual(optionalProps.value);
+  });
+
+  it('renders label if it was set', () => {
+    const mountWrapper = mountWithTheme(<Select {...requiredProps} label={optionalProps.label} />);
+
+    expect(mountWrapper.find('label').first().text()).toEqual(optionalProps.label);
+  });
+
+  it('doesn\'t render label, if it was not set', () => {
+    const mountWrapper = mountWithTheme(<Select {...requiredProps} />);
+
+    expect(mountWrapper.find('label').length).toEqual(0);
+  });
+});
---
+++ front-end/src/components/core/Spinner/Spinner.test.tsx
@@ -0,0 +1,19 @@
+import { shallowWithTheme } from '../../../utils/enzymeHelpersWithPassedTheme';
+import * as React from 'react';
+import Spinner from './index';
+
+// TODO: it's needed to rewrite Spinner component using 'css' emotion helper, insted of 'styled'
+// it's needed to reduce wrapper count and because our enzyme-emotion helpers are not appropriate 
+// for work with this component, and we have to pass theme prop manually
+
+const theme = {
+  bg: 'gray',
+  roller: 'black',
+  stub: 'blue',
+};
+
+describe('Spinner core component:', () => {
+  it('shallow renders without crashing', () => {
+    shallowWithTheme(<Spinner theme={theme} />);
+  });
+});
