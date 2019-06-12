c3cd797ffd0c8b3d0763b736980b1b2f29eab7fe
Uladzislau Kavalski <uladzislau.kavalski@instinctools.ru>
2019-06-12 12:46:21 +0300

    Code cleanup

--- front-end/src/containers/CardList.tsx
+++ front-end/src/containers/CardList.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-import { withTheme } from 'emotion-theming';
 import { Card, ICardProps } from '../components/core/Card/Card';
 import { Text, TextSize, TextWeight } from '../components/core/Text/Text';
 import { Dialog } from '../components/core/Dialog/Dialog';
