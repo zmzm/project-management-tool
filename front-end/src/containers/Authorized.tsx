69bf86c40fec95761f3ddb7cec6bb8946e2f4d4d
Vitali Bantsevich <vitali.bantsevich@instinctools.ru>
2019-06-12 16:53:53 +0300

    change authorized component

--- front-end/src/containers/Authorized.tsx
+++ front-end/src/containers/Authorized.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
-import { Query } from 'react-apollo';
 import { Switch } from 'react-router-dom';
-import { GetUserInfo } from '../graphql/queries/userQueries';
-import { StorageHelper } from '../utils/localStorage';
 import { TopNavigation } from '../components/TopNavigation/TopNavigation';
 import { URLS } from '../consts/urls';
 import { PrivateRoute } from '../components/PrivateRoute/PrivateRoute';
@@ -11,35 +8,16 @@ import Board from './Board';
 
 export default class Authorized extends React.Component {
   public render() {
-    const userFromStorage = StorageHelper.get('user');
-    const userId = this.props['location'].state
-      ? Number.parseInt(this.props['location'].state.user.id)
-      : userFromStorage && Number.parseInt(userFromStorage.id);
-
     return (
-      <Query
-        query={GetUserInfo}
-        variables={{
-          id: userId,
-        }}
-      >
-        {({ data, loading, error }) => {
-          if (loading) return <p>LOADING.....</p>;
-          if (error) return <p>ERROR</p>;
-
-          return (
-            <div>
-              <TopNavigation />
-              <main>
-                <Switch>
-                  <PrivateRoute path={URLS.BOARD} component={Board} />
-                  <PrivateRoute path={URLS.HOME} component={Home} />
-                </Switch>
-              </main>
-            </div>
-          );
-        }}
-      </Query>
+      <div>
+        <TopNavigation />
+        <main>
+          <Switch>
+            <PrivateRoute path={URLS.BOARD} component={Board} />
+            <PrivateRoute path={URLS.HOME} component={Home} />
+          </Switch>
+        </main>
+      </div>
     );
   }
 }
