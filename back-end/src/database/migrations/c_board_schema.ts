75dfa3bc18db0a5856b581c647798a7b2b747621
Uladzislau Kavalski <uladzislau.kavalski@instinctools.ru>
2019-06-14 15:59:17 +0300

    Refactoring model

--- back-end/src/database/migrations/c_board_schema.ts
+++ back-end/src/database/migrations/c_board_schema.ts
@@ -1,28 +1,48 @@
-
 import * as knex from 'knex';
 
 export function up(db: knex) {
   return db.schema
-    .createTable('board', (table) => {
+    .createTable('board', table => {
       table.increments('id').primary();
-      table.string('board_name', 64).unique().notNullable();
+      table
+        .string('board_name', 64)
+        .unique()
+        .notNullable();
       table.boolean('is_closed').notNullable();
-      table.integer('team_id')
+      table
+        .integer('owner_id')
         .unsigned()
         .references('id')
-        .inTable(' team')
+        .inTable('user')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      table
+        .integer('team_id')
+        .unsigned()
+        .references('id')
+        .inTable('team')
         .onDelete('CASCADE')
         .onUpdate('CASCADE');
       table.timestamp('created_at').defaultTo(db.fn.now());
     })
-    .then(() => db(' board').insert([
-      {
-        board_name: 'Test  board',
-        id: 1,
-        is_closed: false,
-        team_id: 1,
-      },
-    ]));
+    .then(() =>
+      db(' board').insert([
+        {
+          board_name: 'Test  board',
+          id: 1,
+          is_closed: false,
+          owner_id: 1,
+          team_id: 1,
+        },
+        {
+          board_name: 'Test personal board',
+          id: 2,
+          is_closed: false,
+          owner_id: 1,
+          team_id: null,
+        },
+      ]),
+    );
 }
 
 export function down(db: knex) {
